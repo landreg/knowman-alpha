ca6e8c7b6fa84a78118fb7b06971765c542bb2cd
Gerard L. Petersen <gerard.petersen@wtg.co.uk>
2015-01-05 11:53:45 +0000

    Permission updates on deploy script

--- deploy/deploy-wtg.sh
+++ deploy/deploy-wtg.sh
@@ -5,6 +5,11 @@ if [ -z "$1" ]; then
     exit
 fi
 
+APACHE_GROUP=$2
+if [ -z "$2" ]; then
+    APACHE_GROUP=apache
+fi
+
 SRC=https://github.com/landreg/knowman-alpha
 DEST="/opt/deploy/landreg/releases"
 BRANCH_NAME=$1
@@ -19,7 +24,15 @@ mkdir -p $VENDOR_PATH
 mkdir -p $CONFIG_PATH
 touch $CONFIG_PATH/parameters.yml
 cd $DESTPATH/app/config && ln -s $CONFIG_PATH/parameters.yml parameters.yml
+mkdir -p $DESTPATH/app/cache && chgrp $APACHE_GROUP $DESTPATH/app/cache && chmod g+s $DESTPATH/app/cache
+mkdir -p $DESTPATH/app/logs && chgrp $APACHE_GROUP $DESTPATH/app/logs && chmod g+s $DESTPATH/app/logs
+chmod +x $DESTPATH/app/console
 cd $DESTPATH && ln -s $VENDOR_PATH vendor && make install
+
+chmod -R 775 $DESTPATH/app/cache
+chmod -R 775 $DESTPATH/app/logs
+
+
 
 
 #parameters.yml
