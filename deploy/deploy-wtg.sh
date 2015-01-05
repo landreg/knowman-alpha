d3b2421332939a164bbf090951a96072b7ed18e2
Gerard L. Petersen <gerard.petersen@wtg.co.uk>
2015-01-05 12:28:59 +0000

    deploy paths updated

--- deploy/deploy-wtg.sh
+++ deploy/deploy-wtg.sh
@@ -12,6 +12,7 @@ fi
 
 SRC=https://github.com/landreg/knowman-alpha
 DEST="/opt/deploy/landreg/releases"
+CURRENT_SYM_PATH="/opt/deploy/landreg"
 BRANCH_NAME=$1
 DESTPATH=$DEST/$(date +%Y%m%d%H%M%S)
 VENDOR_PATH=$DEST/build/vendor
@@ -19,7 +20,7 @@ CONFIG_PATH=$DEST/build/config
 
 set -x
 git clone $SRC -b $BRANCH_NAME $DESTPATH
-cd $DEST && ln -sfn $DESTPATH current
+cd $CURRENT_SYM_PATH && ln -sfn $DESTPATH current
 mkdir -p $VENDOR_PATH
 mkdir -p $CONFIG_PATH
 touch $CONFIG_PATH/parameters.yml
@@ -27,7 +28,7 @@ cd $DESTPATH/app/config && ln -s $CONFIG_PATH/parameters.yml parameters.yml
 mkdir -p $DESTPATH/app/cache && chgrp $APACHE_GROUP $DESTPATH/app/cache && chmod g+s $DESTPATH/app/cache
 mkdir -p $DESTPATH/app/logs && chgrp $APACHE_GROUP $DESTPATH/app/logs && chmod g+s $DESTPATH/app/logs
 chmod +x $DESTPATH/app/console
-cd $DESTPATH && ln -s $VENDOR_PATH vendor && make install
+cd $DESTPATH && ln -s $VENDOR_PATH vendor && composer install
 
 chmod -R 775 $DESTPATH/app/cache
 chmod -R 775 $DESTPATH/app/logs
