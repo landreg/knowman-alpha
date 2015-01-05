63db071be6f5bb674704978742d2beab868a3efd
Gerard L. Petersen <gerard.petersen@wtg.co.uk>
2015-01-05 15:48:57 +0000

    added profile

--- deploy/deploy-wtg.sh
+++ deploy/deploy-wtg.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+. /etc/profile
 if [ -z "$1" ]; then
-    echo usage: $0 branch
+    echo usage: $0 branch [apache user]
     exit
 fi
 
+# Specify
 APACHE_GROUP=$2
 if [ -z "$2" ]; then
     APACHE_GROUP=apache
 fi
 
+# Variable setup
 SRC=https://github.com/landreg/knowman-alpha
 DEST="/opt/deploy/landreg/releases"
 CURRENT_SYM_PATH="/opt/deploy/landreg"
@@ -19,25 +22,29 @@ VENDOR_PATH=$DEST/build/vendor
 CONFIG_PATH=$DEST/build/config
 
 set -x
+# Pull from source
 git clone $SRC -b $BRANCH_NAME $DESTPATH
-cd $CURRENT_SYM_PATH && ln -sfn $DESTPATH current
+
+# Make sure the common vendor and config folders exist
 mkdir -p $VENDOR_PATH
 mkdir -p $CONFIG_PATH
+
+# Create the base config file.
 touch $CONFIG_PATH/parameters.yml
 cd $DESTPATH/app/config && ln -s $CONFIG_PATH/parameters.yml parameters.yml
+
+# Update permissions on all writable and exuctable files and directories
 mkdir -p $DESTPATH/app/cache && chgrp $APACHE_GROUP $DESTPATH/app/cache && chmod g+s $DESTPATH/app/cache
 mkdir -p $DESTPATH/app/logs && chgrp $APACHE_GROUP $DESTPATH/app/logs && chmod g+s $DESTPATH/app/logs
 chmod +x $DESTPATH/app/console
-cd $DESTPATH && ln -s $VENDOR_PATH vendor && composer install
 
+
+# Set the vendor directory to be a symlink and run composer install
+cd $DESTPATH && ln -s $VENDOR_PATH vendor && composer -vvv install
+
+# Make sure the web server can write to it
 chmod -R 775 $DESTPATH/app/cache
 chmod -R 775 $DESTPATH/app/logs
 
-
-
-
-#parameters.yml
-
-
-#vendors folder
-
+# Update symlink to new source
+cd $CURRENT_SYM_PATH && ln -sfn $DESTPATH current
