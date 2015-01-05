3126e491fb385eca0ce87d2c7b05cb38eafb1f53
Gerard L. Petersen <gerard.petersen@wtg.co.uk>
2015-01-05 10:02:25 +0000

    updating deploy scripts

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 install:
 	composer install
+	app/console doctrine:database:create
+	app/console doctrine:phpcr:init:dbal
+	app/console doctrine:phpcr:repository:init
+
+reset:
+	composer install
 	app/console doctrine:database:drop --force
 	app/console doctrine:database:create
 	app/console doctrine:phpcr:init:dbal
--- deploy/deploy-wtg.sh
+++ deploy/deploy-wtg.sh
@@ -9,12 +9,21 @@ SRC=https://github.com/landreg/knowman-alpha
 DEST="/opt/deploy/landreg/releases"
 BRANCH_NAME=$1
 DESTPATH=$DEST/$(date +%Y%m%d%H%M%S)
+VENDOR_PATH=$DEST/build/vendor
+CONFIG_PATH=$DEST/build/config
 
 set -x
 git clone $SRC -b $BRANCH_NAME $DESTPATH
 cd $DEST && ln -sfn $DESTPATH current
-cd $DESTPATH && make install
+mkdir -p $VENDOR_PATH
+mkdir -p $CONFIG_PATH
+touch $CONFIG_PATH/parameters.yml
+cd $DESTPATH/app/config && ln -s $CONFIG_PATH/parameters.yml parameters.yml
+cd $DESTPATH && ln -s $VENDOR_PATH vendor && make install
+
 
 #parameters.yml
 
+
 #vendors folder
+
