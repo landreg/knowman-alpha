f95ba4167adae225dd169777bc566ecdaf94b960
Gerard L. Petersen <gerard.petersen@wtg.co.uk>
2015-01-27 12:10:08 +0000

    Create works

--- src/Landreg/Bundle/KnowmanBundle/Controller/ArticleCRUDController.php
+++ src/Landreg/Bundle/KnowmanBundle/Controller/ArticleCRUDController.php
@@ -45,4 +45,43 @@ class ArticleCRUDController extends CRUDController
         }
         return $returnValue;
     }
+
+
+
+    /**
+     * Create action
+     *
+     * @return Response
+     *
+     * @throws AccessDeniedException If access is not granted
+     */
+    public function createAction()
+    {
+        $returnValue = parent::createAction();
+
+        if ($this->getRestMethod() == 'POST') {
+            $form = $this->admin->getForm();
+            if($form->isValid()) {
+                $id = $this->get('request')->get($this->admin->getIdParameter());
+                $object = $this->admin->getSubject();
+
+                $itemForm = $form->get('existingItem');
+                $item = $itemForm->getData();
+
+                if (!$object) {
+                    throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+                }
+
+                if ($item) {
+                    $newItem = $this->admin->getItemAdmin()->getObject($item);
+                    $object->addItem($newItem);
+
+                    $dm = $this->admin->getModelManager()->getDocumentManager();
+                    $dm->persist($object);
+                    $dm->flush();
+                }
+            }
+        }
+        return $returnValue;
+    }
 }
