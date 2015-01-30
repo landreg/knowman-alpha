6f3e5f835fa9eeb3670c769da046f941ee3136e5
Gerard L. Petersen <gerard.petersen@wtg.co.uk>
2015-01-30 16:04:01 +0000

    Items don't get removed from Articles when re-ordering

---
+++ src/Landreg/Bundle/KnowmanBundle/Form/Extension/CollectionTypeExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Landreg\Bundle\KnowmanBundle\Form\Extension;
+
+use Landreg\Bundle\KnowmanBundle\Form\Listener\CollectionOrderListener;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+
+/**
+ * Extend the sonata collection type to sort the collection so the reordering
+ * is automatically persisted in phpcr-odm.
+ */
+class CollectionTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if ('doctrine_phpcr' != $options['sonata_field_description']->getAdmin()->getManagerType() || ! $options['sonata_field_description']->getOption('sortable')) {
+            return;
+        }
+        $listener = new CollectionOrderListener($options['sonata_field_description']->getName());
+        $builder->addEventListener(FormEvents::SUBMIT, array($listener, 'onSubmit'));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getExtendedType()
+    {
+        return 'sonata_type_collection';
+    }
+}
